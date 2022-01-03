14a61798dbf460ae4d5c37211eb8f8199ef168b9
bywjge <oxl02@qq.com>
2022-01-03 23:34:40 +0800

    20220103

--- SpringMVCDemo/src/main/java/com/hyperskill/springmvcdemo/AddressController.java
+++ SpringMVCDemo/src/main/java/com/hyperskill/springmvcdemo/AddressController.java
@@ -2,12 +2,17 @@ package com.hyperskill.springmvcdemo;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 
 @RestController
 public class AddressController {
     private ConcurrentMap<String, String> addressBook = new ConcurrentHashMap<>();
+//    private Map<String, String> addressBook1 = new HashMap<>();
+//    private Map<String, String> addressBook2 = Collections.synchronizedMap(addressBook1);
 
     @PostMapping("/addresses")
     public void postAddress(@RequestParam String name, @RequestParam String address) {
--- SpringMVCDemo/src/main/java/com/hyperskill/springmvcdemo/UserInfoController.java
+++ SpringMVCDemo/src/main/java/com/hyperskill/springmvcdemo/UserInfoController.java
@@ -1,30 +1,40 @@
 package com.hyperskill.springmvcdemo;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class UserInfoController {
 
-    @PostMapping("/user")
-    public String userStatus(@RequestBody UserInfo user) {
-        if (user.isEnabled()) {
-            return String.format("Hello! %s. Your account is enabled.", user.getName());
-
-        }
-        else {
-            return String.format("Hello! Nice to see you, %s! Your account is disabled",
-                    user.getName()
-            );
-        }
-    }
-
 //    @PostMapping("/user")
-//    public String userStatus(@RequestBody List<UserInfo> userList) {
-//        return  String.format("Added %d users", userList.size());
+//    public String userStatus(@RequestBody UserInfo user) {
+//        if (user.isEnabled()) {
+//            return String.format("Hello! %s. Your account is enabled.", user.getName());
+//
+//        }
+//        else {
+//            return String.format("Hello! Nice to see you, %s! Your account is disabled",
+//                    user.getName()
+//            );
+//        }
 //    }
+    List<UserInfo> userList1 = new ArrayList<>();
+
+    @PostMapping("/user")
+    public String userStatus(@RequestBody List<UserInfo> userList) {
+        userList1 = userList;
+        return  String.format("Added %d users", userList.size());
+    }
+//[{"id":1,"name":"zhangsan","phone":"110","enabled":false},{"id":2,"name":"lisi","phone":"110","enabled":true}]
+
+    @GetMapping("/getuser")
+    public List<UserInfo> getUser() {
+        return userList1;
+    }
 
 }
